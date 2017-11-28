61d9628d4563044238e01a621304cc381320d915
Zach Byers <byerszach@gmail.com>
2017-11-28 15:33:24 -0700

    functional

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+def cc (name, age, hair, eye, gender, alive = true)
+  Person.create(name: name, age: age, hair_color: hair, eye_color: eye, gender: gender, alive: alive)
+end
+
+cc('Zach', 25, 'brown', 'brown', 'm')
+cc('George Washington', 300, 'white', 'blue', 'm', false)
+cc('Bob', 40, 'black', 'green', 'm')
+cc('Martha', 298, 'white', 'hazel', 'f', false)
+cc('Jesse', 28, 'blue', 'brown', 't')
